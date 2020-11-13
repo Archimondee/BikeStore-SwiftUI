115c699bbf8cba5f8b1bbbac1405d239616cc777
Archimondee <kikchiemedia@gmail.com>
2020-11-14 00:20:03 +0700

    Initial Commit

---
+++ BikeStore/BikeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeStoreApp.swift
+//  BikeStore
+//
+//  Created by Gilang Aditya R on 14/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BikeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
